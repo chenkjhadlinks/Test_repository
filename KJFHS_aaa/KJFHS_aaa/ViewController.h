1748d7337d9b5560438c314f2bfaa31cc95e3905
chenkaijie <chenkj@hadlinks.com>
2016-10-20 22:16:07 +0800

    增加一行代码

---
+++ KJFHS_aaa/KJFHS_aaa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KJFHS_aaa
+//
+//  Created by chenkaijie on 16/10/20.
+//  Copyright © 2016年 chenkaijie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KJFHS_aaa/KJFHS_aaa/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  KJFHS_aaa
+//
+//  Created by chenkaijie on 16/10/20.
+//  Copyright © 2016年 chenkaijie. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    
+    
+    NSLog(@"凯杰 福慧顺 ");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
